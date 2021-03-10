a0385cdb32c985bbb0403f59d577113c94bb1951
Cody Tuttle <cody.tuttle.13@gmail.com>
2021-03-10 15:54:34 -0600

    inital commit for week of 3-8

---
+++ week of 2021-03-08/tt_genderbias_3.10.R
@@ -0,0 +1,23 @@
+###########################################
+# Tidy Tuesday week of 3/8
+# Cody R Tuttle
+# Film Gender Bias 
+###########################################
+
+# load pacakges and set options
+
+
+library(tidyverse)
+library(cowplot)
+library(scales)
+library(janitor)
+library(tidytext)
+library(lubridate)
+library(ggtext)
+library(gghighlight)
+library(ggrepel)
+library(purrr)
+
+
+raw_bechdel <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-09/raw_bechdel.csv')
+movies <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-09/movies.csv')
