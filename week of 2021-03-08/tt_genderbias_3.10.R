ab5566528aa545062dcad0e091343f80c96c9ab2
Cody R Tuttle <cody.tuttle.13@gmail.com>
2021-03-10 16:48:01 -0600

    testing

--- week of 2021-03-08/tt_genderbias_3.10.R
+++ week of 2021-03-08/tt_genderbias_3.10.R
@@ -21,3 +21,5 @@ library(purrr)
 
 raw_bechdel <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-09/raw_bechdel.csv')
 movies <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-09/movies.csv')
+
+## change something to test github connection
